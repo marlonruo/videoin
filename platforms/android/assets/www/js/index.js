47ceab313cb99b3571b685b15a136a5df2c06669
marlon ruiz <marlon_ru@hotmail.com>
2017-12-05 13:57:59 -0600

    ajas

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,28 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    onDeviceReady: function() {
+        console.log("Device ready!");
+        try {
+            if (window.device.platform === 'iOS') {
+            }
+        } catch(e) {
+            console.log("Error initializing ios libraries:" + e.message, e)
+        }
+        initVideoChat();
+    },
+};
+
+app.initialize();
